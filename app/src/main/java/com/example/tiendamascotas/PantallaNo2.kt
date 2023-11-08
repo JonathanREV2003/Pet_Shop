5410f863adb8758d4ce8e534fbfca67291bb2f6e
Jonathan Ramiro Elías Vásquez <99297546+JonathanREV2003@users.noreply.github.com>
2023-11-08 12:12:50 -0600

    Barra de opciones y segunda pantalla avance con scaffold

--- app/src/main/java/com/example/tiendamascotas/PantallaNo2.kt
+++ app/src/main/java/com/example/tiendamascotas/PantallaNo2.kt
@@ -3,48 +3,17 @@ package com.example.tiendamascotas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.material3.Button
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.tiendamascotas.ui.theme.TiendaMascotasTheme
-import com.example.tiendamascotas.view.PantallaprincipalII
+import androidx.compose.material3.ExperimentalMaterial3Api
+import com.example.tiendamascotas.view.ScaffolPrincipal2
 
 class PantallaNo2 : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TiendaMascotasTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    PantallaprincipalII()
-                }
-            }
+
+            ScaffolPrincipal2()
+
         }
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview2() {
-    TiendaMascotasTheme {
-        PantallaprincipalII()
     }
 }
