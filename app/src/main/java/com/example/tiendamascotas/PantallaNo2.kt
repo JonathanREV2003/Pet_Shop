859492952fdfc31cc858a1d1b9934c7087963967
Jonathan Ramiro Elías Vásquez <99297546+JonathanREV2003@users.noreply.github.com>
2023-11-07 22:36:50 -0600

    Barra de opciones y segunda pantalla avance

--- app/src/main/java/com/example/tiendamascotas/PantallaNo2.kt
+++ app/src/main/java/com/example/tiendamascotas/PantallaNo2.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tiendamascotas.ui.theme.TiendaMascotasTheme
+import com.example.tiendamascotas.view.PantallaprincipalII
 
 class PantallaNo2 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +33,18 @@ class PantallaNo2 : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    PantallaprincipalII()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview2() {
     TiendaMascotasTheme {
-        Greeting("Android")
+        PantallaprincipalII()
     }
 }
